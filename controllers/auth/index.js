5b31ee22677450a95c38ed6c64851d145e66ae54
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-10-14 18:27:56 +0300

    add fun current

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -1,6 +1,10 @@
 const register = require("./register");
 const login = require("./login");
+const current = require("./getCurrent");
+const logout = require("./logout");
 module.exports = {
     register,
     login,
+    current,
+    logout,
 };
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { ctrlWrapper } = require("../../helpers");
-const {validateBody} = require("../../middlewares");
+const {validateBody, authenticate} = require("../../middlewares");
 const ctrl = require("../../controllers/auth");
 const {schemas} = require("../../models/user");
 
@@ -9,5 +9,8 @@ const router = express.Router();
 
 router.post("/register",validateBody(schemas.registerSchema), ctrlWrapper(ctrl.register));
 router.post("/login",validateBody(schemas.registerSchema), ctrlWrapper(ctrl.login));
+
+router.get("/current", authenticate, ctrlWrapper(ctrl.getCurrent));
+router.get("/logout", authenticate, ctrlWrapper(ctrl.logout))
 
 module.exports = router;
