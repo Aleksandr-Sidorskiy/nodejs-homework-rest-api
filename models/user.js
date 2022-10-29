24d47262ef0b25f3a9edf0a2c2c7de7216c87626
Aleksandr-Sidorskiy <sanu43967@gmail.com>
2022-10-29 21:37:30 +0300

    mail

--- models/user.js
+++ models/user.js
@@ -26,14 +26,14 @@ const userSchema = new Schema({
     type: String,
     require:true,
   },
-  //     verify: {
-  //   type: Boolean,
-  //   default: false,
-  // },
-  // verificationToken: {
-  //   type: String,
-  //   required: [true, 'Verify token is required'],
-  // }, 
+      verify: {
+    type: Boolean,
+    default: false,
+  },
+  verificationToken: {
+    type: String,
+    required: [true, 'Verify token is required'],
+  }, 
 }, { versionKey: false, timestamps: true }
 );
 
@@ -50,9 +50,13 @@ const loginSchema = Joi.object({
   subscription: Joi.string().valid("starter", "pro", "business").required(),
 });
 
+const verifyEmailSchema = Joi.object({
+email: Joi.string().required(),});
+
 const schemas = {
     registerSchema,
-    loginSchema,
+  loginSchema,
+    verifyEmailSchema
 }
 
 const User = model("user", userSchema);
